61cf1fc27b27a6efc57bcbadecfc73be10b1e88f
Parrot Live user <user@localhost.localdomain>
2022-02-17 17:02:19 +0000

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import LoginScreen from "./Screen/Login";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">
+            Prepare With Us
+          </a>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarSupportedContent"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">
+                  Home
+                </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">
+                  Link
+                </a>
+              </li>
+            </ul>
+            <form class="d-flex">
+                <Link className="link" to="/login" style={{marginRight:"5px"}}>Login</Link>
+                <Link className="link" to="/login">Sign Up</Link>
+            </form>
+          </div>
+        </div>
+      </nav>
+      <Routes>
+        <Route path="/login" element={<LoginScreen />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ frontend/src/Screen/Login/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import Login from './component/Login'
+export default function LoginScreen() {
+  return (
+    <Login/>
+  )
+}
